6a291898ef8e9f4e98e93796b613620db8c0ef8e
fernandoprestes <fernandoprestesc@gmail.com>
2022-07-04 19:51:11 -0300

    feat: header title and filter

---
+++ src/components/MovieFilter.vue
@@ -0,0 +1,104 @@
+<script lang="ts" setup>
+  const filterOptionsSelect = [
+    {
+      name: 'action',
+      label: 'ação',
+    },
+    {
+      name: 'action',
+      label: 'Aventura',
+    },
+    {
+      name: 'action',
+      label: 'Animação',
+    },
+    {
+      name: 'action',
+      label: 'Comédia',
+    },
+    {
+      name: 'action',
+      label: 'Crime',
+    },
+    {
+      name: 'action',
+      label: 'Documentário',
+    },
+    {
+      name: 'action',
+      label: 'Drama',
+    },
+    {
+      name: 'action',
+      label: 'Família',
+    },
+    {
+      name: 'action',
+      label: 'Fantasia',
+    },
+    {
+      name: 'action',
+      label: 'História',
+    },
+    {
+      name: 'action',
+      label: 'Terror',
+    },
+    {
+      name: 'action',
+      label: 'Música',
+    },
+    {
+      name: 'action',
+      label: 'Mistério',
+    },
+    {
+      name: 'action',
+      label: 'Romance',
+    },
+    {
+      name: 'action',
+      label: 'Ficção cientifica',
+    },
+    {
+      name: 'action',
+      label: 'Cinema TV',
+    },
+    {
+      name: 'action',
+      label: 'Thriller',
+    },
+    {
+      name: 'action',
+      label: 'Guerra',
+    },
+    {
+      name: 'action',
+      label: 'Faroeste',
+    },
+  ];
+</script>
+<template>
+  <div>
+    <p class="mt-10 mb-4 text-sm font-bold uppercase text-white">Filtre por:</p>
+    <div class="flex w-full flex-wrap gap-3 lg:justify-center">
+      <div
+        v-for="(item, index) in filterOptionsSelect"
+        :key="index"
+        class="w-fit cursor-pointer rounded bg-white px-4 py-2"
+      >
+        <label
+          :for="item.name"
+          class="cursor-pointer select-none font-bold capitalize"
+          >{{ item.label }}</label
+        >
+        <input
+          :id="item.name"
+          type="radio"
+          :name="item.name"
+          class="sr-only"
+        />
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,17 @@
 <script lang="ts" setup></script>
 <template>
-  <div class="grid h-screen place-items-center">
-    <h1 class="text-2xl font-bold">Hello World</h1>
+  <header class="bg-[#5C16C5] py-4">
+    <div class="container flex justify-center md:justify-start">
+      <IconLogo />
+    </div>
+  </header>
+  <div class="flex h-[500px] items-center bg-[#2D0C5E] px-4 lg:px-0">
+    <div class="container md:text-center lg:w-[1120px]">
+      <h1 class="mx-auto text-2xl font-bold leading-tight text-white md:text-5xl lg:w-[780px]">
+        Milhões de filmes, séries e pessoas para descobrir. Explore já.
+      </h1>
+      <MovieFilter />
+    </div>
   </div>
+  <div></div>
 </template>
