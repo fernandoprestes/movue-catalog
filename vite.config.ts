a6cb351f707ae5db8e7adbbe44eeb12a9f8e7daa
fernandoprestes <fernandoprestesc@gmail.com>
2022-07-10 11:58:12 -0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   resolve: {
     alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`,
+      '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
 
