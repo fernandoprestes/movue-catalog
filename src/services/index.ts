5d8751baae9e947a141855e53f331decf6af0c3c
fernandoprestes <fernandoprestesc@gmail.com>
2022-07-16 10:59:24 -0300

    feat: api conection

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,26 +3,26 @@ const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 export class Api {
   static async getAllPopularMovies(page = 1) {
-    const response = await axios.get(`${API_URL}/popular?api_key=${API_KEY}&language=pt-BR&page=${page}&region=BR`);
+    const response = await axios.get(
+      `${API_URL}/movie/popular?api_key=${API_KEY}&language=pt-BR&page=${page}&region=BR`,
+    );
     return response.data;
   }
 
   static async getMoviesWithGenres(query: string, page = 1) {
-    // query = &with_genres=18&with_genres=14
     const response = await axios.get(
-      `${API_URL}/discover/movie?api_key=${API_KEY}&language=pt-BR${query}&page=${page}&region=BR`,
+      `${API_URL}/discover/movie?api_key=${API_KEY}&language=pt-BR&page=${page}&${query}`,
     );
     return response.data;
   }
 
   static async getMovieDetails(movie_id: number) {
-    // query = &with_genres=18&with_genres=14
-    const response = await axios.get(`${API_URL}/${movie_id}?api_key=${API_KEY}&language=pt-BR`);
+    const response = await axios.get(`${API_URL}/movie/${movie_id}?api_key=${API_KEY}&language=pt-BR`);
     return response.data;
   }
 
   static async getMovieCast(movie_id: number) {
-    const response = await axios.get(`${API_URL}/${movie_id}/credits?api_key=${API_KEY}&language=pt-BR`);
+    const response = await axios.get(`${API_URL}/movie/${movie_id}/credits?api_key=${API_KEY}&language=pt-BR`);
     return response.data;
   }
 }
